7259fe0414a602a007dca16b749bbc5227e02a78
ZSedillo <sedillozandro720@gmail.com>
2024-06-11 14:19:04 +0800

    update

--- src/project.jsx
+++ src/project.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+
+import ActiveLearning from './assets/images/activelearning.png'
+import SimpleLoginPage from './assets/images/simpleloginpage.png'
+import GUI from './assets/images/GUI.png'
 import Cat from './assets/images/download-modified.png';
 import BlackJack from './assets/images/Blackjack.jpg';
 import Dice from './assets/images/Dice.jpg';
@@ -13,6 +18,45 @@ function Project() {
       <div className="projects-container">
         <p style={{ color: "#40916c", fontSize: "40px", fontWeight: "bold", textAlign: "center" }}>Projects</p>
         <div className="projects-list">
+
+        <div className="project-option">
+            <Link to="">
+              <div className="image-container">
+                <img src={ActiveLearning} alt="ActiveLearning" />
+                <div className="overlay">
+                  <div className="text">ACTIVE LEARNING PORTAL</div>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+
+
+          <div className="project-option">
+            <Link to="">
+              <div className="image-container">
+                <img src={SimpleLoginPage} alt="SimpleLoginPage" />
+                <div className="overlay">
+                  <div className="text">Simple Login Page</div>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+
+          <div className="project-option">
+            <Link to="">
+              <div className="image-container">
+                <img src={GUI} alt="GUI" />
+                <div className="overlay">
+                  <div className="text">GUI</div>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+
+
           <div className="project-option">
             <Link to="/MyWebsite">
               <div className="image-container">
@@ -54,8 +98,12 @@ function Project() {
             </Link>
           </div>
         </div>
+        <br />
+        <br />
         <p style={{ padding: "20px 0 0 0", color: "#081c15", fontSize: "20px", fontWeight: "bold" }}>
-          These are just a few of the projects I’ve worked on the past; I have many more exciting projects to share!
+        Some of these projects are currently being transferred and stand configured for them to be demonstrated even though they may 
+        be totally complete they might take sometime and require a lot of resources to get them into proper display configuration. 
+        Thank you for your patience
         </p>
       </div>
     </>
